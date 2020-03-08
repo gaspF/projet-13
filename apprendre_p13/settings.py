110554471d9fa4a5ca232c5500508df8aecbfe17
user <fouchegaspard@gmail.com>
2020-03-08 16:14:36 +0100

    settings

--- apprendre_p13/settings.py
+++ apprendre_p13/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
