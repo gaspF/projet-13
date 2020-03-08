852069660aa44d221a1c3c6b83bb01569d277034
user <fouchegaspard@gmail.com>
2020-03-08 15:29:05 +0100

    settings

--- apprendre_p13/settings.py
+++ apprendre_p13/settings.py
@@ -151,9 +151,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
